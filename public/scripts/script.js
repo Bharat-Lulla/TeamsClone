3b5da72559a156b9e7496430e8978d662f18b00a
Bharat Lulla <bharat.2902@gmail.com>
2021-07-08 13:57:33 +0530

    reverting changes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,9 +6,8 @@ const myPeer = new Peer(undefined, {
   port: '443'
 })
 let myStream;
-const myVideo = document.createElement('video');
+const myVideo = document.createElement('video')
 myVideo.muted = true;
-let userVideo;
 
 //making empty object so that user can be find which user gets removed
 const peers = {} 
@@ -52,7 +51,6 @@ navigator.mediaDevices.getUserMedia({
 })
 
 socket.on('user-disconnected', userId => {
-  userVideo.remove();
   if (peers[userId]) peers[userId].close()
 })
 
@@ -69,13 +67,13 @@ function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
 
   const video = document.createElement('video')
-  userVideo = video;
   //this function will take argument that is video stream of that user and we will add that in our code 
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
   })
   //when user leaves the room ie will be used to remove the stream 
   call.on('close', () => {
+    console.log("byeee");
     video.remove()
   })
 
@@ -113,6 +111,7 @@ const muteUnmute = () => {
 }
 
 const playStop = () => {
+  console.log('object')
   let enabled = myStream.getVideoTracks()[0].enabled;
   if (enabled) {
     myStream.getVideoTracks()[0].enabled = false;
@@ -142,6 +141,7 @@ const setUnmuteButton = () => {
 }
 
 const setStopVideo = () => {
+  myVideo.style.display="block";
   const html = `
     <i class="fas fa-video"></i>
     <span>Stop Video</span>
@@ -150,6 +150,7 @@ const setStopVideo = () => {
 }
 
 const setPlayVideo = () => {
+  myVideo.style.display="none";
   const html = `
   <i class="stop fas fa-video-slash"></i>
     <span>Play Video</span>
